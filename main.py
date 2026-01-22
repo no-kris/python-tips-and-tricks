eafb6fe5b3f8d31e7e85b3061805184df2283b12
no-kris <kris.tresk@gmail.com>
2026-01-22 17:24:02 -0500

    update: Added an API endpoint for deleting posts.

--- main.py
+++ main.py
@@ -137,6 +137,16 @@ def update_post_partial(
     return post
 
 
+@app.delete("/api/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+    if not post:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Post not found.")
+    db.delete(post)
+    db.commit()
+
+
 @app.post(
     "/api/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED
 )
